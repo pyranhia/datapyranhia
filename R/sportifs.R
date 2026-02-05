6bb1a623c3563027bbab41368ec2bd64fb16f442
Thelma Panaiotis <thelma@crans.org>
2026-02-05 16:50:40 +0100

    Ajout des sportifs

---
+++ R/sportifs.R
@@ -0,0 +1,13 @@
+#' Données de performance des sportifs
+#'
+#' Jeu de données synthétiques pour illustrer les principaux concepts du machine learning.
+#'
+#' @format Un tibble avec 500 lignes et 4 variables :
+#' \describe{
+#'   \item{heures}{dbl Heures d’entraînement par semaine}
+#'   \item{experience}{dbl Annés d’expérience}
+#'   \item{score}{dbl Score de performance}
+#'   \item{set}{chr Dataset split : `"Train"` ou `"Test"`}
+#' }
+#'
+"sportifs"
